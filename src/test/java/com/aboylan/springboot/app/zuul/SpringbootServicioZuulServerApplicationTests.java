aa31eadc632681262b35c0f21442cd02e954b3f2
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-12 18:42:46 -0600

    first commit

---
+++ src/test/java/com/aboylan/springboot/app/zuul/SpringbootServicioZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aboylan.springboot.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
